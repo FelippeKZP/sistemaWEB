8195785ee3a6202c36b976719ffbd5de946af71a
Felippe <felipekzp0@gmail.com>
2018-08-16 00:51:59 -0300

    Atualização No Projeto

---
+++ views/redefinir.php
@@ -0,0 +1,14 @@
+<h1>Redefinir A Senha</h1>
+
+<div class="formulario">
+    <form method="POST">
+        <label>Digite A Nova Senha:</label>
+        <input type="password" id="senha" name="senha" /><br/>
+        <label>Digite Novamente A Senha:</label>
+        <input type="password" id="digite" name="digite" /><br/>
+        
+        <input type="submit" id="bt" value="Mudar Senha"/>
+        <a id="btVoltar" href="<?php echo BASE_URL;?>login">Voltar</a>
+        
+    </form>
+</div>
